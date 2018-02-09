201ad8cf44d39533ea24fc5c883912cf565c14b4
Swasidhant <swasidhant.chowdhury@yatra.com>
2018-02-09 20:44:41 +0530

    Initial Commit

---
+++ ForeverSingle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForeverSingle
+//
+//  Created by Swasidhant Chowdhury on 09/02/18.
+//  Copyright © 2018 Swasidhant Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
